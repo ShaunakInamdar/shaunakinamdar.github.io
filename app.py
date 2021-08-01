93fcc05e5e5a54bab85558c360e6b941aa764733
ShaunakInamdar <shaunak.inamdar@gmail.com>
2021-08-01 14:49:45 +0530

    added redirect

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 
 app = Flask(__name__, template_folder="templates", static_folder = "static")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return redirect("https://shaunak-inamdar.medium.com/")
 
 if __name__ == "__main__":
     app.run()
